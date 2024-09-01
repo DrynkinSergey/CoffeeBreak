dbcb3746b2babcd98e12577a5d474d733eb2b0d3
Sergey <sergsmile29@gmail.com>
2024-09-01 22:13:01 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,63 @@
-import { FC, useState } from 'react'
-import Countdown, { zeroPad } from 'react-countdown'
+import { FC, useState } from 'react';
+import Countdown, { zeroPad } from 'react-countdown';
 type Props = {
-	setIsStartBreak: (p: boolean) => void
-}
+  setIsStartBreak: (p: boolean) => void;
+};
 const Completionist: FC<Props> = ({ setIsStartBreak }) => {
-	return (
-		<button
-			className='border-4 px-4 py-2 my-2 rounded-md border-black hover:bg-black hover:text-white transition-all duration-300'
-			onClick={() => setIsStartBreak(true)}
-		>
-			Start break
-		</button>
-	)
-}
+  return (
+    <button
+      className='border-4 px-4 py-2 my-2 rounded-md border-black hover:bg-black hover:text-white transition-all duration-300'
+      onClick={() => setIsStartBreak(true)}
+    >
+      Start break
+    </button>
+  );
+};
 export const App = () => {
-	const renderer = ({ minutes, seconds, completed }: { minutes: number; seconds: number; completed: boolean }) => {
-		if (completed) {
-			return <Completionist setIsStartBreak={setIsStartBreak} />
-		} else {
-			return minutes >= 10 ? (
-				<span className='text-green-700 text-6xl'>
-					{zeroPad(minutes)}:{zeroPad(seconds)}
-				</span>
-			) : (
-				<span className='text-red-700 text-6xl'>
-					{zeroPad(minutes)}:{zeroPad(seconds)}
-				</span>
-			)
-		}
-	}
-	const [isStartBreak, setIsStartBreak] = useState(false)
-	const [time, setTime] = useState('30')
+  const renderer = ({ minutes, seconds, completed }: { minutes: number; seconds: number; completed: boolean }) => {
+    if (completed) {
+      return <Completionist setIsStartBreak={setIsStartBreak} />;
+    } else {
+      return minutes >= 10 ? (
+        <h1 className=' glitch-text text-green-700 text-6xl'>
+          {zeroPad(minutes)}:{zeroPad(seconds)}
+        </h1>
+      ) : (
+        <h1 className='glitch-text text-red-700 text-6xl'>
+          {zeroPad(minutes)}:{zeroPad(seconds)}
+        </h1>
+      );
+    }
+  };
+  const [isStartBreak, setIsStartBreak] = useState(false);
+  const [time, setTime] = useState('30');
 
-	return (
-		<div className="italic bg-[url('./assets/bg.jpg')] h-screen bg-cover flex justify-start items-center">
-			<div className='ml-10  border-4 rounded-2xl p-10 bg-white/50  '>
-				<h1 onClick={() => setTime(prompt('Enter time:') ?? '30')} className='text-black text-5xl font-bold'>
-					We have a coffee break
-				</h1>
-				<div className='text-black/90 text-3xl font-bold'>
-					{!isStartBreak && <Completionist setIsStartBreak={setIsStartBreak} />}
-					{isStartBreak && (
-						<Countdown date={Date.now() + +time * 60 * 1000} precision={3} renderer={renderer}>
-							<h1>Go ahead!!</h1>
-						</Countdown>
-					)}
-				</div>
-			</div>
-		</div>
-	)
-}
+  return (
+    <div>
+      <div className='relative h-screen flex justify-start items-center'>
+        <video autoPlay muted loop className='absolute inset-0 w-full h-full object-cover'>
+          <source src='/bg.mp4' type='video/mp4' />
+        </video>
+        <div className='absolute top-[28%] left-[20%] z-10'>
+          <div className='ml-10  border-3 rounded-2xl p-10 backdrop-blur-sm bg-white/10  '>
+            <h1
+              onClick={() => setTime(prompt('Enter time:') ?? '30')}
+              className='glitch-text text-black text-2xl font-bold'
+              data-text='We have a coffee break'
+            >
+              We have a coffee break
+            </h1>
+            <div className='text-black/90 text-1xl font-bold'>
+              {!isStartBreak && <Completionist setIsStartBreak={setIsStartBreak} />}
+              {isStartBreak && (
+                <Countdown date={Date.now() + +time * 60 * 1000} precision={3} renderer={renderer}>
+                  <h1 className='glitch-text'>Go ahead!!</h1>
+                </Countdown>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
